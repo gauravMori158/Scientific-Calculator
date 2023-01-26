48f4c100006858166b1bf1de1fa9081aa0951f8d
gauravMori158 <77385932+gauravMori158@users.noreply.github.com>
2023-01-26 16:40:50 +0530

    functionality updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 class Calculator {
+  
   constructor(dataPreviousText, dataCurrentText) {
     this.dataCurrentText = dataCurrentText;
     this.dataPreviousText = dataPreviousText;
@@ -21,6 +22,7 @@ class Calculator {
     if (this.previousOperand !== "") {
       this.compute();
     }
+   
     this.operation = operation;
     if (operation == "10x") {
       this.operation = "10^";
@@ -35,18 +37,26 @@ class Calculator {
     } else if (operation == "n!") {
       this.operation = "!";
     }
-
+    else if (operation == "exp") {
+      this.operation = ".e+";
+    }
+    else if (operation == "|x|") {
+      this.operation = "abs";
+    }
     this.previousOperand = this.currentOperand;
     this.currentOperand = "";
   }
 
   compute() {
     let computation;
-    const prev = parseFloat(this.previousOperand);
+    let prev = parseFloat(this.previousOperand);
     const current = parseFloat(this.currentOperand);
-    if (isNaN(prev) && isNaN(current)) return;
+    //if (isNaN(prev) && isNaN(current)) return;
     console.log(this.operation);
-
+     if(isNaN(prev)){
+      prev =0;
+     }
+    console.log("prev" + prev);
     switch (this.operation) {
       case "+":
         computation = prev + current;
@@ -88,24 +98,51 @@ class Calculator {
         break;
       case "!":
         if (prev < 0) {
-            computation = -1;
-        }
-
-        
-        else if (prev === 0) {
-            computation = 1;
-        }
-   
-        else {
+          computation = -1;
+        } else if (prev === 0) {
+          computation = 1;
+        } else {
           let fact = 1;
           for (let i = 1; i <= prev; i++) {
             fact *= i;
           }
           computation = fact;
         }
-       
 
         break;
+        case ".e+":
+        computation = prev* Math.pow(10,current);
+
+        break;
+        case "abs":
+        computation =   (prev < 0) ? (-1)*prev : prev;
+
+        break;
+        case "sin":
+        computation =   Math.sin(current);
+
+        break;
+        case "cos":
+        computation =   Math.tan(current);
+
+        break;
+        case "tan":
+        computation =   Math.sin(current);
+
+        break;
+        case "sec":
+        computation =  1/ Math.cos(current);
+
+        break;
+        case "csc":
+        computation =   1/Math.sin(current);
+
+        break;
+        case "cot":
+        computation =  1/ Math.tan(current);
+
+        break;
+        
 
       default:
         return;
@@ -133,8 +170,26 @@ class Calculator {
     }
   }
   updateDisplay() {
+    const func =[ 'sin','cos','tan','sec','cot','csc'];
     this.dataCurrentText.innerText = this.getDisplayNumber(this.currentOperand);
     if (this.operation != null) {
+        if ( func.includes(this.operation)){
+          this.dataPreviousText.innerText = `${this.operation} (${this.getDisplayNumber(
+            this.currentOperand
+          )})`;
+          this.dataCurrentText.innerText='';
+          return;
+          
+        }
+        else if(this.operation =='abs')
+        {this.dataPreviousText.innerText = `${this.operation} (${this.getDisplayNumber(
+          this.previousOperand
+        )})`;
+        this.dataCurrentText.innerText='';
+        return;
+
+        }
+
       this.dataPreviousText.innerText = `${this.getDisplayNumber(
         this.previousOperand
       )} ${this.operation}`;
@@ -142,17 +197,22 @@ class Calculator {
       this.dataPreviousText.innerText = "";
     }
   }
+  clearPrev()
+  {
+    this.previousOperand ='';
+  }
 }
 
 const numberButton = document.querySelectorAll("[data-number]");
+const numberButtonPi = document.querySelectorAll("[data-number-pi]");
 const operationButton = document.querySelectorAll("[ data-operation ]");
-
+const numberButtonE = document.querySelectorAll("[data-number-e]");
 const equalsButton = document.querySelector("[data-equals]");
 const dataAllClearButton = document.querySelector("[data-all-clear]");
 const clearButton = document.querySelector("[data-delete]");
 const dataPreviousText = document.querySelector("[data-prvious]");
 const dataCurrentText = document.querySelector("[data-current]");
-
+ 
 const calculator = new Calculator(dataPreviousText, dataCurrentText);
 
 numberButton.forEach((button) => {
@@ -161,7 +221,20 @@ numberButton.forEach((button) => {
     calculator.updateDisplay();
   });
 });
+ 
+numberButtonPi.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(22/7);
+    calculator.updateDisplay();
+  });
+});
 
+numberButtonE.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(2.7182818284);
+    calculator.updateDisplay();
+  });
+});
 operationButton.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.chooseOperation(button.innerText);
@@ -172,13 +245,24 @@ operationButton.forEach((button) => {
 equalsButton.addEventListener("click", (button) => {
   calculator.compute();
   calculator.updateDisplay();
+  calculator.clearPrev();
+
+   
 });
 dataAllClearButton.addEventListener("click", (button) => {
   calculator.clear();
   calculator.updateDisplay();
+
 });
 
 clearButton.addEventListener("click", (button) => {
   calculator.delete();
   calculator.updateDisplay();
 });
+function displayNone(){
+   
+  document.getElementById("rowBlock").style.display = "none";
+}
+function displayTrigonometry(){
+  document.getElementById("rowBlock").style.display = "flex";
+}
